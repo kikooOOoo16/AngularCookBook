435eb496523e4e3a7bfb0f0dca307955e744540d
Kristijan Pavlevski <kikoooooo_@hotmail.com>
2020-11-21 22:28:44 +0100

    Fix navbar dropdown not showing and minor navbar UI fixes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,11 +6,45 @@ import {map} from 'rxjs/operators';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipes.actions';
 import {faBook, faShoppingBasket, faSignInAlt, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+import {animate, keyframes, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  // animations: [
+  //   trigger('navbarDropdownMenuLink', [
+  //     state('active', style({
+  //       opacity: '1',
+  //       transform: 'translateX(0)'
+  //     })),
+  //     transition('void => *', [
+  //       animate(1000, keyframes([
+  //         style({
+  //           transform: 'translateX(-100px)',
+  //           opacity: '0',
+  //           offset: '0'
+  //         }),
+  //         style({
+  //           transform: 'translateX(-50px)',
+  //           opacity: '0.5',
+  //           offset: '0.3'
+  //         }),
+  //         style({
+  //           transform: 'translateX(-20px)',
+  //           opacity: '1',
+  //           offset: '0.8'
+  //         }),
+  //         style({
+  //           transform: 'translateX(0px)',
+  //           opacity: '1',
+  //           offset: '1'
+  //         }),
+  //         ]
+  //       ))
+  //     ])
+  //   ] )
+  // ]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   faBook = faBook;
@@ -18,16 +52,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
   faSignIn = faSignInAlt;
   faSignOut = faSignOutAlt;
   private userSub: Subscription;
-  private isDropdownOpen = false;
+  // isDropdownOpen = false;
+  isDropdownActive = '';
   isAuthenticated = false;
-  @ViewChild('navbarDropdown', {static: true}) navbarDropdown: ElementRef;
+  @ViewChild('navbarDropdownMenuLink', {static: true}) navbarDropdownMenuLink: ElementRef;
+  @ViewChild('navbarMainListDiv', {static: true}) navbarMainListDiv: ElementRef;
   @ViewChild('navbar', {static: true}) navbar: ElementRef;
   @HostListener('window:scroll', ['$event'])
   toggleClass(event) {
     if (window.pageYOffset > 0) {
-      this.renderer.addClass(this.navbar.nativeElement, 'affix')
+      this.renderer.addClass(this.navbar.nativeElement, 'affix');
+      this.renderer.addClass(this.navbarDropdownMenuLink.nativeElement, 'affix')
     } else {
       this.renderer.removeClass(this.navbar.nativeElement, 'affix')
+      this.renderer.removeClass(this.navbarDropdownMenuLink.nativeElement, 'affix')
     }
     // console.log('Scroll Event', window.pageYOffset );
   }
@@ -63,12 +101,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   toggleDropdown() {
-    if (this.isDropdownOpen) {
-      this.renderer.removeClass(this.navbarDropdown.nativeElement, 'show');
-      this.isDropdownOpen = false;
+    if (this.isDropdownActive === 'active') {
+      this.renderer.removeClass(this.navbarDropdownMenuLink.nativeElement, 'active');
+      this.renderer.removeClass(this.navbarMainListDiv.nativeElement, 'show_list');
+      this.isDropdownActive = '';
     } else {
-      this.renderer.addClass(this.navbarDropdown.nativeElement, 'show');
-      this.isDropdownOpen = true;
+      this.renderer.addClass(this.navbarDropdownMenuLink.nativeElement, 'active');
+      this.renderer.addClass(this.navbarMainListDiv.nativeElement, 'show_list');
+      this.isDropdownActive = 'active';
     }
   }
 }
