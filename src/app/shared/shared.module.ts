16b5d93031aa05c2dc92b2b5f4e378275680e820
Kristijan Pavlevski <kikoooooo_@hotmail.com>
2020-12-02 22:14:55 +0100

    Edit alert component's CSS, add alert for adding ingredients from recipes detail component to shopping list component, create a new directive stop-propagation, add CSS media query styles to auth component for multiple screen sizes, add CSS media query styles to header component, other UI fixes...

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import {AlertComponent} from './alert/alert.component';
 import {CommonModule} from '@angular/common';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { TrackScrollDirective } from './trackscroll.directive';
+import { StopPropagationDirective } from './stop-propagation.directive';
 
 @NgModule({
   declarations: [
@@ -14,20 +15,22 @@ import { TrackScrollDirective } from './trackscroll.directive';
     DropdownDirective,
     PlaceholderDirective,
     TrackScrollDirective,
+    StopPropagationDirective,
   ],
   imports: [
     CommonModule,
     FontAwesomeModule
   ],
-  exports: [
-    CommonModule,
-    FontAwesomeModule,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective,
-    DropdownDirective,
-    TrackScrollDirective
-  ],
+    exports: [
+        CommonModule,
+        FontAwesomeModule,
+        LoadingSpinnerComponent,
+        AlertComponent,
+        PlaceholderDirective,
+        DropdownDirective,
+        TrackScrollDirective,
+        StopPropagationDirective
+    ],
   entryComponents: [
     AlertComponent
   ]
